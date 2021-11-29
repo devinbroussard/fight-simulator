567f180219cd58efb0d471f438144316e19fd2bd
Devin <devin.ray.broussard@gmail.com>
2021-11-29 16:54:32 -0600

    Adding entities in main

--- IntroToCPP/IntroToCPP/main.cpp
+++ IntroToCPP/IntroToCPP/main.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
+#include <cstring>
+#include "Entity.h"
 
 int main()
 {
-
-	char firstName[32];
-	char lastName[32];
-	char fullName[64] = "";
-
-	std::cout << "Please enter your name: ";
-	std::cin >> firstName;
-
-	system("cls");
-
-	std::cout << "Now please enter your last name: ";
-	std::cin >> lastName;
-
-	system("cls");
-
-	strcat_s(fullName, firstName);
-	strcat_s(fullName, lastName);
-
-	std::cout << "Your name is: " << fullName << "\n";
-	system("pause");
+	Entity wompus = Entity('W', 100, 10, -10);
+	return 0;
 }
 	
